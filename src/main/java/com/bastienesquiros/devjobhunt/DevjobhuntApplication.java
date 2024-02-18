a6607a5883da57382143f12bfceb6ef4af2d2d70
Bastien Esquiros <bastien.esquiros@hotmail.com>
2024-02-18 20:25:34 +0100

    init

---
+++ src/main/java/com/bastienesquiros/devjobhunt/DevjobhuntApplication.java
@@ -0,0 +1,13 @@
+package com.bastienesquiros.devjobhunt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevjobhuntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevjobhuntApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bastienesquiros/devjobhunt/DevjobhuntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bastienesquiros.devjobhunt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevjobhuntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
